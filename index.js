f26c47725ea8845a67f38682c41ce6c8b723229f
Niv Sardi <xaiki@evilgiggle.com>
2018-04-22 16:59:55 -0300

    initial commit
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -3,161 +3,90 @@
 const Provider = require('butter-provider');
 const moment = require('moment');
 const API = require('node-youtubeapi-simplifier');
-const debug = require('debug')('butter-provider-youtube')
+const debug = require('debug')('butter-provider-gdocs')
+const gsjson = require('google-spreadsheet-to-json');
 
 const defaultConfig = {
-    name: 'youtube',
-    uniqueId: 'ytid',
-    tabName: 'YouTube',
+    name: 'gdocs',
+    uniqueId: 'id',
+    tabName: 'Google Docs',
     argTypes: {
-        channel: Provider.ArgType.STRING,
-        apiKey: Provider.ArgType.STRING,
-        baseUrl: Provider.ArgType.STRING
+        gdocsSpreadSheetId: Provider.ArgType.STRING,
+        gdocsHeaderStart: Provider.ArgType.NUMBER,
+        youtubeApiKey: Provider.ArgType.STRING,
+        youtubeBaseUrl: Provider.ArgType.STRING
     },
     defaults: {
-        apiKey: 'AIzaSyARQAHCYNuS7qi3mUxu0pgc4FjEBkOrx3U',
-        baseUrl: 'https://media.youtube.de/public'
+        gdocsSpreadSheetId: '1cR-jUrhPNA-tJfMyilDvvwvXkWYFMHlld81CWMXkFdg',
+        gdocsHeaderStart: 2,
+        youtubeApiKey: 'AIzaSyARQAHCYNuS7qi3mUxu0pgc4FjEBkOrx3U',
+        youtubeBaseUrl: 'https://media.youtube.de/public'
     }
 }
 
-// reimplement flatten to get rid of underscore/lodash
-function flatten(e) {
-    return e.reduce((a, c) => (a.concat(c)), [])
-}
-
-function getBestThumb(th) {
-    var res = [
-        'maxres',
-        'high',
-        'standard',
-        'medium',
-        'default'
-    ]
-
-    while (res.length) {
-        var r = res.shift();
-        if (th[r]) {
-            return th[r].url
-        }
+const translateData = (data) => {
+    const translate = {
+        'título': 'title',
+        'gênero': 'genre',
+        'ano': 'year',
+        'sinopse': 'synopsis',
+        'tempo': 'runtime'
     }
 
-    return null
-}
-
-function formatForButter(data) {
-    var channel = data.channel,
-        playlists = data.playlists;
-
-    var id = channel.channelId;
-    var updated = moment(channel.publishedAt);
-    var year = updated.year();
-    var img = getBestThumb(channel.avatar);
-
-    return {
-        results: [
-            {
-                type: Provider.ItemType.TVSHOW,
-                ytid: id,
-                title: channel.title,
-                synopsis: channel.description,
-                year: year,
-                poster: img,
-                backdrop: img,
-                slug: id,
-                rating: {
-                    hated: 0,
-                    loved: 0,
-                    votes: 0,
-                    percentage: 0,
-                    watching: 0
-                },
-                num_seasons: playlists.length,
-                last_updated: updated.unix()
-            }
-        ],
-        hasMore: false
-    }
-}
-
-function generatePlaylistTorrents(pl) {
-    debug('NOT IMPLEMENTED')
-
-    return pl
-}
-
-function formatPlaylistForButter(pl, idx, videos) {
-    return videos.map((vid, vidx) => {
-        var date = moment(vid.publishedAt);
-
-        return {
-            torrents: generatePlaylistTorrents(vid),
-            watched: {
-                watched: false
-            },
-            first_aired: date.unix(),
-            date_based: false,
-            overview: vid.description,
-            title: vid.title,
-            episode: vidx + 1,
-            season: idx + 1,
-            tvdb_id: vid.videoId
-        }
+    Object.keys(translate).map(k => {
+        data[translate[k]] = data[k]
+        delete(data[k])
     })
+
+    return data
 }
 
-module.exports = class YouTube extends Provider {
+const formatItemForButter = (item) => ({
+    ...item,
+    type: Provider.ItemType.MOVIE,
+    //XXX(xaiki): should calculate something usefull here
+    rating: item.statistics.likeCount,
+    subtitles: [],
+    torrents: [],
+    genre: [item.genre],
+    backdrop: item.thumbnails.high.url,
+    poster: item.thumbnails.high.url,
+    trailer: item.linkYoutube,
+    synopsis: item.synopsis || item.description || 'no synopsis provided'
+})
+
+const formatForButter = (items) => ({
+    results: items.map(formatItemForButter),
+    hasMore: true
+})
+
+const yt2ID = (link) => {
+    let match = [
+        /youtu.be\/([-\w]+)/,
+        /watch\?v=([-\w]+)/
+    ].map(r => link.match(r))
+     .filter(r => r)
+
+    return match[0][1]
+}
+
+module.exports = class GDocs extends Provider {
     constructor (args, config = defaultConfig) {
         super(args, config)
 
-        this.channel = this.args.channel
-        this.apiKey = this.args.apiKey
-        this.baseUrl = this.args.baseUrl
-        this.regex = {}
-
-        Object.keys(args).forEach((k) => {
-            var m = k.match('(.*)Regex');
-
-            if (!m) {
-                return;
-            }
-
-            this.regex[m[1]] = new RegExp(args[k])
-        })
-
-        debug(args, this.channel, this.apiKey);
+        this.youtubeApiKey = this.args.youtubeApiKey
+        this.youtubeBaseUrl = this.args.youtubeBaseUrl
+        this.spreadsheetId = this.args.gdocsSpreadSheetId
+        this.headerStart = this.args.gdocsHeaderStart
 
         this.API = API
 
-        this.API.setup(this.apiKey)
-        this.playlists = this.API.playlistFunctions
-                             .getPlaylistsForUser(this.channel)
-                             .then((playlists) => {
-                                 if (Object.keys(this.regex).length < 1) {
-                                     return playlists;
-                                 }
-
-                                 return playlists.filter((p) => {
-                                     var found = Object.keys(this.regex).map((field) => {
-                                         var val = p[field];
-                                         let regex = this.regex[field]
-
-                                         if (!val) {
-                                             return false
-                                         }
-
-                                         return val.match(regex) ? true : false;
-                                     })
-
-                                     return found.indexOf(true) > -1
-                                 })
-                             })
-
-        this.channel = this.API.channelFunctions.getDetailsForUser(this.channel);
+        this.API.setup(this.youtubeApiKey)
     }
 
     queryTorrents (filters = {}) {
         var params = {};
-//        var genres = '';
+        //        var genres = '';
         params.sort = 'seeds';
         params.limit = '50';
 
@@ -178,65 +107,37 @@ module.exports = class YouTube extends Provider {
             params.sort = filters.sorter;
         }
 
-        if (this.fetchData) {
-            return Promise.resolve(this.fetchData);
-        }
+        return this.getGDocsData()
+                   .then(this.translateData)
+                   .then(this.getVideoInfo.bind(this))
+    }
 
-        return (Promise.all([
-            this.playlists,
-            this.channel
-        ]))
-            .then((data) => {
-                this.fetchData = {
-                    playlists: data[0],
-                    channel: data[1]
-                }
+    getGDocsData() {
+        return gsjson({
+            spreadsheetId: this.spreadsheetId,
+            headerStart: this.headerStart,
+            hash: 0,
+        })
+    }
 
-                return this.fetchData
-            })
-            .catch((err) => {
-                debug('youtube', 'error', err)
-            })
+    translateData(items) {
+        return items.map(translateData)
+    }
+
+    getVideoInfo(items) {
+        return this.API.videoFunctions.getDetailsForVideoIds(
+            items.map(item => (yt2ID(item.linkYoutube)))
+        ).then(info => (items.map((item, i) => ({
+            ...item,
+            id: info[i].id,
+            thumbnails: info[i].thumbnails,
+            statistics: info[i].statistics,
+            description: info[i].description
+        }))))
     }
 
     fetch(filters = {}) {
         return this.queryTorrents(filters)
                    .then(formatForButter)
-                   .then((data) => {
-                       if (!filters.keywords) {
-                           return data;
-                       }
-
-                       var re = new RegExp(filters.keywords.replace(/\s/g, '\\s+'), 'gi')
-                       if (re.match(data.results[0].title)) {
-                           return data
-                       }
-
-                       return {results: [],
-                           hasMore: false}
-                   })
-    }
-
-    getPlaylistsVideos(playlists) {
-        return Promise.all(playlists.map((pl, idx) => this.API.playlistFunctions.getVideosForPlaylist(pl.playlistId)
-                       .then((videos) => (formatPlaylistForButter(pl, idx, videos)))))
-    }
-
-    detail(id, oldData) {
-        var data = this.fetchData
-        var updated = moment(oldData.updated)
-
-        return this.getPlaylistsVideos(data.playlists)
-                   .then((videos) => Object.assign(oldData, {
-                       country: '',
-                       network: 'YouTube Media',
-                       status: 'finished',
-                       num_seasons: data.playlists.length,
-                       runtime: 30,
-                       last_updated: updated.unix(),
-                       __v: 0,
-                       genres: ['FIXME'],
-                       episodes: flatten(videos)
-                   }))
     }
 }
